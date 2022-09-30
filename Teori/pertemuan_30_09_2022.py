4a1a7e0ddb2b1a643727baef16665bbb17300dcd
Shalahuddin-Abdul <94779696+Shalahuddin-Abdul@users.noreply.github.com>
2022-09-30 14:40:51 +0700

    Add files via upload

---
+++ Teori/pertemuan_30_09_2022.py
@@ -0,0 +1,43 @@
+# list1 = [1, 2, 2, "Hello"]
+# # atau
+# list2 = 1, 2, 3, "Hello"
+
+# print(list1)
+# print(list2)
+
+# list1.append(2)
+# print(list1)
+
+# list1.pop(2)
+# print(list1)
+
+# list1.insert(2, 'x')
+# print(list1)
+
+# list1.extend(list2)
+# print(list1)
+
+
+# a = set('hanif')
+# b = set('rafli')
+# print(a^b)
+
+# mySet={1, 2, 3, 3, 3}
+# mySet2={6, 4, 5}
+# print(mySet.copy())
+# print(mySet.union(mySet2))
+
+# tel = {'hanif': 2117051032, 'rafli': 2117051067}
+# print(tel['hanif'])
+
+# tup = 1, 2, 3,-2 ,3, 6
+# print(tup)
+
+myList= [2117051083, 2117051032, 2117051067]
+mySet3 = {2117051083, 2117051032, 2117051067}
+myTup = 2117051083, 2117051032, 2117051067
+
+print(myList)
+print(mySet3)
+print(myTup)
+
