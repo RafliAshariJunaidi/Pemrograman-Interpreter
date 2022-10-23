6c9fb407bc8e89e1d3c5321eb843f37792dcdeaf
Rafli Ashari Junaidi <92414621+RafliAshariJunaidi@users.noreply.github.com>
2022-10-23 09:02:14 +0700

    Update UTS_PI.py

--- Praktikum/UTS/UTS_PI.py
+++ Praktikum/UTS/UTS_PI.py
@@ -1,45 +1,20 @@
-import pandas as pd
-
-gpuData = pd.read_csv("https://raw.githubusercontent.com/Shalahuddin-Abdul/Pemrograman-Interpreter/main/Praktikum/UTS/All_GPUs.csv")
-cpuData = pd.read_csv("https://raw.githubusercontent.com/Shalahuddin-Abdul/Pemrograman-Interpreter/main/Praktikum/UTS/Intel_CPUs.csv")
-
-
-gpuDedicated = gpuData.copy().loc[gpuData['Integrated']=='No']
-pRateAvg = gpuDedicated["Pixel_Rate(GPixel/s)"].mean()
-pRateMax = gpuDedicated["Pixel_Rate(GPixel/s)"].max()
-pRateMin = gpuDedicated["Pixel_Rate(GPixel/s)"].min()
-pRateSkew = gpuDedicated["Pixel_Rate(GPixel/s)"].skew()
+import time
+from loginFunct import login
+from recFunct import filters
+from recFunct import gpuRec
 
 needDict = {
-    "Browsing" : 1,
-    "Gaming Ringan" : 2,
-    "Gaming Berat" : 3,
-    "Mini PC" : 4,
-    "Kerja" : 5,
-    "Sekolah" : 6,
-    "Video Editing" : 7
+    1 : "Browsing",
+    2 : "Gaming Ringan",
+    3 : "Gaming Berat",
+    4 : "Mini PC",
+    5 : "Kerja",
+    6 : "Sekolah",
+    7 : "Video Editing"
 }
 
-# def perfRating(i):
-#     pRateRating = gpuDedicated["Pixel_Rate(GPixel/s)"][i]/pRateAvg*5
-#     return int(pRateRating)
-def filters(cat, x):
-    # new = gpuDedicated.drop(cat, axis=1)
-    output = gpuDedicated.copy().loc[gpuDedicated[cat]>x]
-    print(output[["Name", cat]])
-
+login()
+time.sleep(3)
 print(needDict)
 x = input("Masukkan Kebutuhan anda: ")
-category = "Memory(MB)"
-compParam = 1
-def gpuRec(x):
-    if x == 1:
-        filters("Memory(MB)", 1024) 
-    elif x == 2:
-        filters("Memory(MB)", 2048)
-    elif x == 3:
-        filters("Memory(MB)", 4096)
-    elif x == 4:
-        filters("Memory(MB)", 2048)
-
-# print(gpuDedicated)
+gpuRec(int(x))
